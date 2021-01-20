e7f4543f291e991bc6cf89241dba373eeb655c42
EsriDuPlessis <77608300+EsriDuPlessis@users.noreply.github.com>
2021-01-20 11:07:50 +0200

    Type Basics
    Just an overview on how to use different types of data and how to declare them. 
    Also, how to implement a function is shown as well.

---
+++ sandbox.js
@@ -0,0 +1,14 @@
+var character = 'mario'; //declare that variable 'character' is of type string
+var age = 30; //declare that variable 'age' is of type number
+var isBlackBelt = false; //declare that variable 'isBlackBelt' is of type boolean
+//TS recognises what type of data you declare and checks throughout
+//the code what type of data you used.
+//You can however change the variables by doing the following:
+character = 'luigi';
+age = 40;
+isBlackBelt = true;
+//declare a function named 'diameter' of type number
+var circ = function (diameter) {
+    return diameter * Math.PI; //return a value 
+};
+console.log(circ(7.5)); //show the output of 'circ' with input of 7.5
---
+++ sandbox.ts
@@ -0,0 +1,18 @@
+let character = 'mario'; //declare that variable 'character' is of type string
+let age = 30; //declare that variable 'age' is of type number
+let isBlackBelt = false; //declare that variable 'isBlackBelt' is of type boolean
+
+//TS recognises what type of data you declare and checks throughout
+//the code what type of data you used.
+//You can however change the variables by doing the following:
+
+character = 'luigi';
+age = 40;
+isBlackBelt = true;
+
+//declare a function named 'diameter' of type number
+const circ = (diameter: number) => { 
+    return diameter * Math.PI; //return a value 
+};
+
+console.log(circ(7.5)); //show the output of 'circ' with input of 7.5
