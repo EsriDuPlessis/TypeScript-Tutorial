50788fac26503fbda2463e5cbb5efc825739c8a6
EsriDuPlessis <77608300+EsriDuPlessis@users.noreply.github.com>
2021-01-21 17:54:33 +0200

    L8 - Function Signatures
    This shows how to give functions certain signatures.

---
+++ public/sandbox.js
@@ -0,0 +1,26 @@
+"use strict";
+// example 1
+var greet;
+//Here you give the variable 'greet' a function signature,
+//meaning that you declare which type of data you input
+greet = function (name, greeting) {
+    console.log('${name} says ${greeting}');
+};
+//The ${} notation allows you to output the data from the function as a string type
+// example 2
+var calc;
+//Function signature
+//Note that this function is set to give a certain data type as output
+calc = function (numOne, numTwo, action) {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo; //The code will result in error if you don't make sure that 
+    } //a data type 'number' is given as output
+};
+// example 3
+var logDetails;
+logDetails = function (ninja) {
+    console.log('${ninja.name} is ${ninja.age} years old');
+};
---
+++ src/sandbox.ts
@@ -0,0 +1,29 @@
+// example 1
+let greet: (a: string, b: string) => void;
+//Here you give the variable 'greet' a function signature,
+//meaning that you declare which type of data you input
+
+greet = (name: string, greeting: string) => {
+    console.log('${name} says ${greeting}');
+}
+//The ${} notation allows you to output the data from the function as a string type
+
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+//Function signature
+//Note that this function is set to give a certain data type as output
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if (action === 'add') { 
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo; //The code will result in error if you don't make sure that 
+    }                           //a data type 'number' is given as output
+}
+
+// example 3
+let logDetails: (obj: {name: string, age: number}) => void;
+
+logDetails = (ninja: {name: string, age: number}) => {
+    console.log('${ninja.name} is ${ninja.age} years old');
+}
